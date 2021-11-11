d6fc4c79c01ea3a1ffcbe7fcaffb9ed10cbc0509
SajeevreddyKarri <92910602+SajeevreddyKarri@users.noreply.github.com>
2021-11-11 12:24:03 +0530

    Add files via upload

---
+++ creating a square program .c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+   int s, a, p;
+   printf("enter the side");
+   scanf("%d", &s);
+   a= s*s;
+   p= 4*a;
+   printf("area of the square is %d\n",a);
+   printf("perimetet of the square is %d", p);
+}
